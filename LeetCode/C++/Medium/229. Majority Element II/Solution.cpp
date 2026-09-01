436233ee85aab912fe8f279223428fdce9bf55a8
Aastha Jethwa <136359437+AasthaJethwa@users.noreply.github.com>
2026-09-01 20:09:20 +0530

    Add Medium C++ Solution for 229. Majority Element II from LeetCode

---
+++ LeetCode/C++/Medium/229. Majority Element II/Solution.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int n = nums.size();
+
+        int cnt1 = 0, cnt2 = 0;
+        int el1 = INT_MIN, el2 = INT_MIN;
+
+        // Find potential candidates
+        for (int i = 0; i < n; i++) {
+
+            if (cnt1 == 0 && nums[i] != el2) {
+                el1 = nums[i];
+                cnt1 = 1;
+            }
+            else if (cnt2 == 0 && nums[i] != el1) {
+                el2 = nums[i];
+                cnt2 = 1;
+            }
+            else if (nums[i] == el1) {
+                cnt1++;
+            }
+            else if (nums[i] == el2) {
+                cnt2++;
+            }
+            else {
+                cnt1--;
+                cnt2--;
+            }
+        }
+
+        // Verify candidates
+        cnt1 = 0;
+        cnt2 = 0;
+
+        for (int num : nums) {
+            if (num == el1) cnt1++;
+            if (num == el2) cnt2++;
+        }
+
+        vector<int> ans;
+        int mini = n / 3 + 1;
+
+        if (cnt1 >= mini)
+            ans.push_back(el1);
+
+        if (cnt2 >= mini && el1 != el2)
+            ans.push_back(el2);
+
+        return ans;
+    }
+};
